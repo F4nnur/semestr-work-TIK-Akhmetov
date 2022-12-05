428bb8c824c7b91ce3125399c445dd222a26e7ee
Fannur <Fann1kF@yandex.ru>
2022-12-05 12:23:02 +0300

    +fix shennon

--- Shennon/Shennon.py
+++ Shennon/Shennon.py
@@ -2,8 +2,10 @@ import math
 from numpy import cumsum
 from operator import itemgetter
 
-def shennon(file):
+def shennon(file, user_word):
     input_file = open(file, "r")
+    list_of_user_word = list(user_word)
+    print(list_of_user_word)
     letters = [line.rstrip() for line in input_file.readlines(2)]
     numbers = [line.rstrip() for line in input_file.readlines(1)]
     lines_of_letters = letters[0].split(' ')
@@ -12,19 +14,20 @@ def shennon(file):
     dict_n_l = dict(zip(lines_of_letters, lines_of_numbers))
     soted_dict = dict(sorted(dict_n_l.items(), key=itemgetter(1), reverse=True))
     
+    symbols_of_sorted_dict = []
     lst_of_nmb = []
     for_log = []
 
-    for values in soted_dict.values():
+    for key, values in soted_dict.items():
         lst_of_nmb.append(values)
+        symbols_of_sorted_dict.append(key)
+
     for_log = lst_of_nmb.copy()
 
     summ_iter = cumsum(lst_of_nmb) - lst_of_nmb
     int_numbers_lst = []
     for elem in summ_iter:
          int_numbers_lst.append(int((elem % 1 * 100)))
-    # print(int_numbers_lst)
-
     
     lst_for_binary = []
     
@@ -34,14 +37,16 @@ def shennon(file):
     logged_lst = []    
     for elem in for_log:
         logged_lst.append(math.ceil(math.log2(1/elem)))
-    print(logged_lst)
 
     prefinal_dict = dict(zip(lst_for_binary, logged_lst))
     final_dict = []
     for key, value in prefinal_dict.items():
         final_dict.append(str(key)[:value])
     
-    return final_dict    
+    new_dict = dict(zip(symbols_of_sorted_dict, final_dict))
+    
+    new_dict_for_elems = {e: new_dict[e] for e in list_of_user_word}
+    return new_dict_for_elems
         
     
-print(shennon('input.txt'))
+print(shennon('input.txt', 'acdbe'))
